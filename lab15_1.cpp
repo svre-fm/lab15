14fce3d6bdd03bebd1a842d32792faca705a8bba
svre-fm <firmoioi96@gmail.com>
2025-01-28 20:08:04 +0700

    complete

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,28 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
 
+	int a = 5;
+	char b = 'A';
+	char &c = b;
+	int *x = &a;
+	char *y = &b;
+	int **z = &x;
+
+	cout << a << " " << b << " " << c << " " << (void*)x  << " " << (void*)y << " " << (void*)z << endl;
+	cout << &a << " " << (void*)&b << " " << (void*)&c << " " << (void*)&x  << " " << (void*)&y << " " << (void*)&z << endl;
 	
+	c = 'F';
+	cout << a << " " << b << " " << c << " " << (void*)x  << " " << (void*)y << " " << (void*)z << endl;
 	
+	*y = 'W';
+	cout << a << " " << b << " " << c << " " << (void*)x  << " " << (void*)y << " " << (void*)z << endl;
+
+	*x = 6;
+	cout << a << " " << b << " " << c << " " << (void*)x  << " " << (void*)y << " " << (void*)z << endl;
+
+	**z = 7;
+	cout << a << " " << b << " " << c << " " << (void*)x  << " " << (void*)y << " " << (void*)z << endl;
 	return 0;
 }
